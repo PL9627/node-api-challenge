ac8fe55624f0bb22939970e0c3ce6d1f75106d5a
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-12 16:31:58 -0700

    finished actions GET /:id call

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -9,11 +9,15 @@ router.get("/", (req, res, next) => {
         res.status(200).json(action)
     })
     .catch(err => next(err))
-})
+});
 
-router.get("/:id", (req, res, next => {
-
-}))
+router.get("/:id", (req, res, next) => {
+    actions.get(req.params.id)
+    .then(action => {
+        res.json(200).json(action)
+    })
+    .catch(err => next(err))
+});
 
 router.post("/", (req, res, next) => {
 
