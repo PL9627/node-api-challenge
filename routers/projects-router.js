028f90cfcefcf5718953f523d0f53febd04110b3
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-12 16:57:07 -0700

    routers/projects-router.js
    finished part of prjects GET / call

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -4,7 +4,12 @@ const projects = require("../data/helpers/projectModel");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    
+    projects
+    .get()
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) => next(err));
 })
 
 router.get("/:id", (req, res, next => {
