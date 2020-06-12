b60fe6a28994ae870577027616c5c78954ed33b7
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-12 16:37:45 -0700

    finished actions POST / call

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -20,7 +20,17 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+    if (!req.body) {
+        res.status(400).json({
+            message: "missing data in the body"
+        })
+    }
 
+    actions.insert(req.body)
+    .then(action => {
+        res.status(201).json(action)
+    })
+    .catch(err => next(err))
 })
 
 router.put("/:id", (req, res, next) => {
