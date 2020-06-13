5ee66c4b1ce2b8603c6a8ddb2d1f69e8ff7a73ae
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-12 17:01:50 -0700

    finished projects GET / call

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -4,28 +4,34 @@ const projects = require("../data/helpers/projectModel");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    projects
+  projects
     .get()
-    .then((action) => {
-      res.status(200).json(action);
+    .then((project) => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "This post does not exist",
+        });
+      }
     })
     .catch((err) => next(err));
-})
+});
 
-router.get("/:id", (req, res, next => {
+router.get("/:id", (req, res, next) => {
 
-}))
+});
 
 router.post("/", (req, res, next) => {
 
-})
+});
 
 router.put("/:id", (req, res, next) => {
 
-})
+});
 
 router.delete("/:id", (req, res, next) => {
     
-})
+});
 
 module.exports = router;
