cb09a55c56f17d2d2465be20003a57e653f9d545
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-12 17:09:34 -0700

    finished projects POST / call

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -19,30 +19,37 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-    projects
+  projects
     .get(req.params.id)
     .then((project) => {
-        if (project) {
-            res.json(200).json(project);
-        } else {
-            res.status(404).json({
-                message: "The project with the specified ID does not exist"
-            })
-        }
+      if (project) {
+        res.json(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist",
+        });
+      }
     })
     .catch((err) => next(err));
 });
 
 router.post("/", (req, res, next) => {
+  if (!req.body) {
+    res.status(400).json({
+      message: "missing data in the body",
+    });
+  }
 
+  projects
+    .insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => next(err));
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", (req, res, next) => {});
 
-});
-
-router.delete("/:id", (req, res, next) => {
-
-});
+router.delete("/:id", (req, res, next) => {});
 
 module.exports = router;
